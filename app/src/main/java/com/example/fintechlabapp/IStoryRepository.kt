54984da722598c5efcc914455e86405471f27675
Ansar Kadyrov <j423252@gmail.com>
2021-01-31 21:06:19 +0300

    Update repository.

--- app/src/main/java/com/example/fintechlabapp/IStoryRepository.kt
+++ app/src/main/java/com/example/fintechlabapp/IStoryRepository.kt
@@ -1,7 +1,9 @@
 package com.example.fintechlabapp
 
 import com.example.fintechlabapp.api.StoryResponse
+import retrofit2.http.Path
 
 interface IStoryRepository {
-    suspend fun getStory(): Result<StoryResponse>
+    suspend fun getRandomStory(): Result<StoryResponse>
+    suspend fun getStoriesByType(type: String, page: Int): Result<StoryResponse>
 }
--- app/src/main/java/com/example/fintechlabapp/StoryRepository.kt
+++ app/src/main/java/com/example/fintechlabapp/StoryRepository.kt
@@ -7,10 +7,22 @@ import kotlinx.coroutines.withContext
 
 class StoryRepository(private val storiesApi: StoriesApi): IStoryRepository {
 
-    override suspend fun getStory(): Result<StoryResponse> =
+    override suspend fun getRandomStory(): Result<StoryResponse> =
         withContext(Dispatchers.IO) {
             try {
                 val response = storiesApi.getRandomStory()
+                Result.Success(response)
+            }
+            catch (t: Throwable) {
+                Result.Error("Oops..looks like network failure!")
+            }
+        }
+
+    override suspend fun getStoriesByType(type: String, page: Int): Result<StoryResponse> =
+        withContext(Dispatchers.IO) {
+            try {
+                val stories = storiesApi.getStories(type, page).stories
+                val response = stories.first() //TODO: replace with rundom number
                 Result.Success(response)
             }
             catch (t: Throwable) {
